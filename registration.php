58eb114769a312a30fcacf385b6cb8da01468b6e
Timothy <tjyu1040@gmail.com>
2015-03-26 15:11:12 -0400

    Cleaned up registration details.

--- registration.php
+++ registration.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title></title>
     <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    <link rel="stylesheet" type="text/css" href="css/form.css" />
     <link rel="stylesheet" type="text/css" href="bower_components/jquery-ui/themes/smoothness/jquery-ui.min.css">
 </head>
 <body>
@@ -12,35 +13,44 @@
 <div class="wrapper">
 
     <?php
-    include "logoutButton.html";
+    include "loginForm.html";
     include "header.html"; ?>
 
     <div class="container">
         <?php
-        if(!empty($_POST['username']) && !empty($_POST['password'])) {
+        if (!empty($_POST['username']) && !empty($_POST['password'])) {
             $username = mysql_real_escape_string($_POST['username']);
             $password = md5(mysql_real_escape_string($_POST['password']));
             $email = mysql_real_escape_string($_POST['email']);
+            $first_name = mysql_real_escape_string($_POST['first_name']);
+            $last_name = mysql_real_escape_string($_POST['last_name']);
+            $background = mysql_real_escape_string($_POST['background']);
+            $education = mysql_real_escape_string($_POST['education']);
+            $skills = mysql_real_escape_string($_POST['skills']);
+            $role = mysql_real_escape_string($_POST['role']);
 
-            $check_username = mysql_query("SELECT * FROM users WHERE user_name = '".$username."'");
+            $check_username = mysql_query("SELECT * FROM users WHERE user_name = '" . $username . "'");
 
-            if(mysql_num_rows($check_username) == 1) {
+            if (mysql_num_rows($check_username) == 1) {
                 echo "<h1>Error</h1>";
                 echo "<p>Sorry, that username is taken. Please try again.</p>";
                 include "registrationForm.html";
             } else {
-                $register_query = mysql_query("INSERT INTO users (user_name, password, user_email, is_verified, is_alumni) VALUES('".$username."', '".$password."', '".$email."', false, true)");
-                if($register_query) {
-                    echo "<h1>Success</h1>";
-                    echo "<p>Your account was successfully created. You may now login.</p>";
+                $register_query = mysql_query("INSERT INTO users (user_name, password, user_email, is_verified, is_alumni) VALUES('" . $username . "', '" . $password . "', '" . $email . "', false, true)");
+                $get_user_id = mysql_query("");
+                if ($register_query) {
+                    $profile_query = mysql_query("INSERT INTO profiles (first_name, last_name, background, education, skills, role) VALUES('" . $first_name . "', '" . $last_name . "', '" . $background . "', '" . $education . "', '" . $skills . "', '" . $role . "'");
+                    if ($profile_query) {
+                        echo "<h1>Success</h1>";
+                        echo "<p>Your account was successfully created. You may now login.</p>";
+                    }
                 } else {
                     echo "<h1>Error</h1>";
                     echo "<p>Sorry, your registration failed. Please try again.</p>";
                     include "registrationForm.html";
                 }
             }
-        }
-        else {
+        } else {
             include "registrationForm.html";
         }
         ?>
