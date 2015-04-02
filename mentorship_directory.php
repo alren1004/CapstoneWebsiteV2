9285f20ec20a084414d8cea933139e3cb89e5da8
Timothy <tjyu1040@gmail.com>
2015-04-02 14:28:49 -0400

    Added listing of mentorships.

--- mentorship_directory.php
+++ mentorship_directory.php
@@ -13,7 +13,7 @@
 <div class="wrapper">
 
     <?php
-    if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])){
+    if (!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])) {
         // Logged in.
         include "logoutButton.html";
     } else {
@@ -26,19 +26,45 @@
     <div class="container">
 
         <?php
-        if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])){
+        if (!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])) {
             // Logged in.
-        $get_role = mysql_query("SELECT * FROM profiles WHERE profile_id = " . $_SESSION['profile_id'] . "");
+            $get_role = mysql_query("SELECT * FROM profiles WHERE profile_id = " . $_SESSION['profile_id'] . "");
 
+            if ($get_role) {
+                $role = mysql_fetch_array($get_role)['role'];
+                if (strcasecmp($role, "Mentor") == 0) {
+                    include "mentorRegistrationForm.html";
+                }
+            }
 
-        if ($get_role) {
-            $role = mysql_fetch_array($get_role)['role'];
-            if(strcasecmp($role, "Mentor") == 0){
-                include "mentorRegistrationForm.html";
+            $get_mentorships = mysql_query("SELECT * FROM mentors");
+            if ($get_mentorships) {
+                echo
+                "<h1 align='center'>Available Mentorships</h1>
+                            <table align='center'>
+                                <tr>
+                                    <th>Number:</th>
+                                    <th>Profile:</th>
+                                    <th>Availability:</th>
+                                    <th>Years:</th>
+                                    <th>Field:</th>
+                                    <th>Start Date:</th>
+                                    <th>End Date:</th>
+                                </tr>";
+                // fetch each record in result set
+                for ($counter = 0; $row = mysql_fetch_row($get_mentorships); ++$counter) {
+                    // build table to display results
+                    print("<tr>");
+
+                    foreach ($row as $key => $value)
+                        print("<td>$value</td>");
+
+                    print("</tr>");
+                }
+                echo "</table>";
             } else {
                 echo "You are not a mentor.";
             }
-        }
 
         } else {
             // Not logged in.
